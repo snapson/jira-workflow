9e0dee9e3897ae9e5930913fe9f110d7f61c962a
Nikita Zubarets <zubarets@gmail.com>
2014-07-07 02:19:28 +0300

    Updates - node http & reload was added; - jade templating added;

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 
 var tasker = require('./tasker');
-var app = require('express')();
+var express = require('express')
+var http = require('http')
+var jade = require('jade');
+var path = require('path');
+var reload = require('reload');
+var templates = path.resolve(__dirname, 'templates');
+var app = express();
+var server;
 
 /*
+  TODO: Update livereload
+  TODO: Create package.json
   TODO: Create route for single page
   TODO: Create view for index route
-  TODO: Use jade for template
-  TODO: Use mocha for tests
-  TODO: Use chai for tests
+  TODO: Write tests
+  TODO: Include sass in project
 
   IDEAS: Add caching to project
   IDEAS: Add gziping
+  IDEAS: Maybe use Grunt or Bower?
 */
 
-app.listen(3000);
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.resolve(__dirname, 'assets')));
 
 app.get('/', function(req, res) {
-  res.json(tasker.getSubtasks());
+  res.send( jade.renderFile(path.resolve(templates, 'index.jade'), { tasker: tasker }) );
+});
+
+
+
+
+// For autoreload purposes
+server = http.createServer(app);
+reload(server, app);
+server.listen(app.get('port'), function(){
+  console.log("Web server listening on port " + app.get('port'));
 });
