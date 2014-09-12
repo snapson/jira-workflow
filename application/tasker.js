599f336faddbd493fd4f1318b110504b5b785004
Nikita Zubarets <zubarets@gmail.com>
2014-09-12 19:10:31 +0300

    Some work with multiple task creation

--- application/tasker.js
+++ application/tasker.js
@@ -15,6 +15,9 @@ var login = require('./login');
 
 var tasker = module.exports = {
   project: null,
+  projects: null,
+  isEqualProjects: false,
+  allWorkIsDone: false,
   baseURI: 'https://contegixapp1.livenation.com/jira/browse/',
   subtasks: {
     DEV: [
@@ -23,7 +26,8 @@ var tasker = module.exports = {
       { summary: 'Modify tests', description: 'Modify neccessary tests.' },
       { summary: 'Code review', description: 'Pass code review.' },
       { summary: 'Test cases', description: 'Write some test cases' },
-      { summary: 'Merge to master', description: 'Merge code to astro gitlab.' }
+      { summary: 'Merge to master', description: 'Merge code to astro gitlab.' },
+      { summary: 'Merge to develop', description: 'Merge code to svn develop' }
     ],
     QA: [
       { summary: 'Acceptance testing' },
@@ -32,12 +36,21 @@ var tasker = module.exports = {
     ]
   },
   getAllSubtasks: function() { return this.subtasks; },
-  getURI: function() { return path.join(this.baseURI, this.project); },
+  getURI: function() { return false; /* path.join(this.baseURI, this.project); */ },
+  setProjects: function(done) {
+    var that = this;
+
+    _(that.projects).forEach(function(prjct) {
+      that.setProject(prjct, done);
+    });
+
+    this.allWorkIsDone = true;
+  },
   setProject: function(parentTask, done) {
-    subtask.fields.project.key = parentTask.split('-')[0].toUpperCase();
-    subtask.fields.parent.key = parentTask.toUpperCase();
-    this.project = parentTask.toUpperCase();
-    done(); // Become from init
+    subtask.fields.project.key = parentTask.split('-')[0].toUpperCase(); // ALOHA
+    subtask.fields.parent.key = parentTask.toUpperCase(); // ALOHA-757
+    this.project = parentTask.toUpperCase(); // ALOHA-757
+    done(); // Become from setProjects
   },
   setFields: function(type, summ) {
     var current = _.find(this.subtasks[type], { summary: summ });
@@ -86,56 +99,102 @@ var tasker = module.exports = {
         that.createSingleTask(that.selectedSubtasks.QA, 'QA', cb);
       }
     ], function(err, resp) {
-      console.log('Complete creating all tasks');
-      done(); // Become from init
+
+      if (this.isEqualProjects || this.allWorkIsDone) {
+        console.log('Complete creating all tasks');
+        done(); // Become from init
+      } else {
+        console.log('Another project will go');
+        that.createTasks(done);
+      }
+
     });
   },
   createSingleTask: function(tasks, type, done) {
-    var that = this;
-    var jiraConnect = login.connect;
-    var count = 0;
+    // var that = this;
+    // var jiraConnect = login.connect;
+    // var count = 0;
 
-    async.whilst(
-      function () { return count <= (tasks.length - 1); },
-      function (callback) {
-        jiraConnect.addNewIssue(that.setFields(type, tasks[count].summary), function(err, body) {
-          if (!err) {
-            console.log('Subtask is successfully created');
-            count++;
-            callback();
-          } else {
-            console.log('Some error is occured');
-          }
-        });
-      },
-      function (err) {
-        err && (console.log('Some error is occured', err));
-        !err && (done()); // Become from createTasks
-      }
-    );
+    // async.whilst(
+    //   function () { return count <= (tasks.length - 1); },
+    //   function (callback) {
+    //     jiraConnect.addNewIssue(that.setFields(type, tasks[count].summary), function(err, body) {
+    //       if (!err) {
+    //         console.log('Subtask is successfully created');
+    //         count++;
+    //         callback();
+    //       } else {
+    //         console.log('Some error is occured');
+    //       }
+    //     });
+    //   },
+    //   function (err) {
+    //     err && (console.log('Some error is occured', err));
+    //     !err && (done()); // Become from createTasks
+    //   }
+    // );
+
+    done();
   },
-  checkData: function(data, next) {
+  testProjects: function(data) {
     var parentTest = /\w+\-\d+/g;
 
-    if (_.isEmpty(data.parentTask) || !parentTest.test(data.parentTask)) {
-      next( new Error('Parent task must not be empy and must be like aloha-747.') ); // Become from init
-    }
-
+    return _.map(data, function(task) {
+      return task.match(parentTest) && task;
+    });
+  },
+  checkSubtasks: function (data, next) {
     if (_.indexOf(_.values(data), 'on') < 0) {
       next( new Error('You must select one or more tasks.') ); // Become from init
     }
 
     next();
   },
+  checkEqual: function(data, next) {
+    var that = this;
+    var uniqProjects = _.uniq(data.parentTask, function(string) {
+      return string.toLowerCase();
+    });
+    var testUniq = that.testProjects(uniqProjects);
+
+    this.projects = _.compact(testUniq);
+    this.isEqualProjects = _.isEqual(that.projects.length, 1);
+
+    if (_.contains(testUniq, null)) {
+      next( new Error('Parent task must be like aloha-747.') ); // Become from init
+    }
+
+    next(); // Become from init
+  },
+  splitProjects: function(data, next) {
+    var that = this;
+
+    if (_.isEmpty(data.parentTask)) {
+      next( new Error('Parent task must not be empy') ); // Become from init
+    }
+
+    var withOutSpaces = data.parentTask && data.parentTask.replace(/\s/g, '');
+    var parentTasks = withOutSpaces.split(',');
+
+    data.parentTask = parentTasks;
+
+    next(); // Become from init
+  },
   init: function(data, done) {
     var that = this;
 
     async.series([
       function(cb) {
-        that.checkData(data, cb);
+        that.splitProjects(data, cb);
       },
       function(cb) {
-        that.setProject(data.parentTask, cb);
+        that.checkEqual(data, cb);
+      },
+      function(cb) {
+        that.checkSubtasks(data, cb);
+      },
+      function(cb) {
+        that.setProjects(cb);
       },
       function(cb) {
         that.setSelectedSubtasks(data, cb);
